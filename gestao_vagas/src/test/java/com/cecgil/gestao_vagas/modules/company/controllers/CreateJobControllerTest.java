d1c326191b5e8b63e4a7df8756257ee56015e779
Carlos <dudigil6@gmail.com>
2025-02-14 00:48:21 -0300

    Criação TestUtils e Avanço do JobControllerTest

---
+++ gestao_vagas/src/test/java/com/cecgil/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -0,0 +1,59 @@
+package com.cecgil.gestao_vagas.modules.company.controllers;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.cecgil.gestao_vagas.modules.company.dto.CreateJobDTO;
+import com.cecgil.gestao_vagas.utils.TestUtils;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment =  WebEnvironment.RANDOM_PORT)
+public class CreateJobControllerTest {
+
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+        .webAppContextSetup(context)
+        .apply(SecurityMockMvcConfigurers.springSecurity())
+        .build();
+    }
+
+    @Test
+    public void shouldBeAbleToCreateNewJob() throws Exception {
+
+        var createJobDTO = CreateJobDTO.builder()
+        .benefits("BENEFITS_TEST")
+        .description("DESCRIPTION_TEST")
+        .level("LEVEL_TEST")
+        .build();
+
+        var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(TestUtils.objectToJSON(createJobDTO))
+        .header("Authorization", TestUtils.generateToken(UUID.randomUUID(), "VAGAS_@123#"))
+        )
+        .andExpect(MockMvcResultMatchers.status().isOk());
+
+        System.out.println(result);
+    }
+    
+}
---
+++ gestao_vagas/src/test/java/com/cecgil/gestao_vagas/utils/TestUtils.java
@@ -0,0 +1,38 @@
+package com.cecgil.gestao_vagas.utils;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestUtils {
+
+    public static String objectToJSON(Object obj) {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String generateToken(UUID companyId, String secret) {
+        
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+
+        var token = JWT.create().withIssuer("vagas")
+                    .withSubject(companyId.toString())
+                    .withExpiresAt(expiresIn)
+                    .withClaim("roles", Arrays.asList("COMPANY"))
+                    .sign(algorithm);
+        
+        return token;
+    }
+    
+}
